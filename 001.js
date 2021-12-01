6c73651dfd1c3a55c2d5b0a0bbb1a07b23b1b81e
woobba94 <jwj3199@naver.com>
2021-12-01 19:27:20 +0900

    211201

---
+++ 001.js
@@ -0,0 +1,13 @@
+var nums = [100, 200, 300, 400, 500];
+
+// // 1번 방법
+// nums.length -= 2;
+
+// // 2번 방법
+// nums.pop();
+// nums.pop();
+
+// // 3번 방법
+// nums = nums.slice(0, 3);
+
+console.log(nums);
---
+++ 003.js
@@ -0,0 +1,5 @@
+var arr = [100, 200, 300];
+console.log(typeof arr);
+
+// object 라니.. ㅠㅠㅠㅠㅠ
+// number를 골랐읍니다.
